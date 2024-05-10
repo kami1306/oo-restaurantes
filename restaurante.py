a5a69d158ea86949e29a9c70791a41ab8e8fdc6c
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-10 11:25:14 -0300

    Add files via upload

--- restaurante.py
+++ restaurante.py
@@ -1,13 +1,21 @@
 class Restaurante:
+    restaurantes = []
+
     def __init__(self,nome,categoria):
         self.nome = nome
         self.categoria=categoria
         self.ativo= False
+        Restaurante.restaurantes.append(self)
+    
+    def _str_(self):
+       return f'{self.nome} | {self.categoria}'
 
-        Restaurante_praca = Restaurante('Praça','Gourmet')
-        Restaurante_pizza = Restaurante('pizza','italiana')
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f' {Restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
-        Restaurantes =[Restaurante_pizza,Restaurante_praca]
 
-        print(vars(Restaurante_praca))
-        print(vars(Restaurante_pizza))
+Restaurante_praca = Restaurante('Praça','Gourmet')
+Restaurante_pizza = Restaurante('pizza', 'categoria')
+
+Restaurante.listar_restaurantes()
