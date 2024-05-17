237bc560ed9a9cc7243ecd6aa8085690fe632ea0
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-17 11:25:02 -0300

    Add files via upload

--- restaurante.py
+++ restaurante.py
@@ -1,21 +1,29 @@
 class Restaurante:
-    restaurantes = []
 
-    def __init__(self,nome,categoria):
-        self.nome = nome
-        self.categoria=categoria
-        self.ativo= False
-        Restaurante.restaurantes.append(self)
-    
-    def _str_(self):
-       return f'{self.nome} | {self.categoria}'
+restaurantes = []
 
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f' {Restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+def init__(self, nome,categoria):
 
+self.nome nome
 
-Restaurante_praca = Restaurante('Praça','Gourmet')
-Restaurante_pizza = Restaurante('pizza', 'categoria')
+self.categoria = categoria self._ativo = False
+
+Restaurante.restaurantes.append(self)
+
+def str eturn ^ self): f:fself } | {self.categoria}
+
+for restaurante in Restaurante. restaurantes
+
+@property
+
+def ativo(self):
+
+def listar_restaurantes():
+
+: | print(f'(restaurante.nome.ljust(25)) | restaurante.categoria.ljust(25) | (restaurante.ativo)')
+
+return if self. ativo else 'o'
+
+restaurante_praca = Restaurante('Praça', 'Gourmet') restaurante_pizza Restaurante('Pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
